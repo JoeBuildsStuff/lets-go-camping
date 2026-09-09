6cf4db41d0d669242f69fb088247117d194cc8e0
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2026-09-08 22:06:28 -0700

    feat: update dependencies and enhance styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
